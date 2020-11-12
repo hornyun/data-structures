98a7f867761805436b3e7a3f27da166c25e3e08d
hornYun <hornyun@sina.com>
2020-11-12 23:44:49 +0800

    complete shell sort

--- src/main/java/com/hornyun/day23/ShellSortMain.java
+++ src/main/java/com/hornyun/day23/ShellSortMain.java
@@ -33,13 +33,14 @@ public class ShellSortMain {
     public static void shellSort1(int[] array) {
         if (ArrayUtils.isNotEmpty(array)) {
             int temp;
-            for (int interval = array.length / 2; interval > 0; interval /= 2) {
+            int divider = 2;
+            for (int interval = array.length / divider; interval > 0; interval /= divider) {
                 for (int i = interval; i < array.length; i++) {
                     for (int j = i - interval; j >= 0; j -= interval) {
                         if (array[j] > array[j + interval]) {
                             temp = array[j];
                             array[j] = array[j + interval];
-                            array[j+interval] = temp;
+                            array[j + interval] = temp;
                         }
                     }
                 }
@@ -49,14 +50,15 @@ public class ShellSortMain {
 
     public static void shellSort2(int[] array) {
         if (ArrayUtils.isNotEmpty(array)) {
-            for (int interval = array.length / 2; interval > 0; interval /= 2) {
+            int divider = 2;
+            for (int interval = array.length / divider; interval > 0; interval /= divider) {
                 for (int i = interval; i < array.length; i++) {
                     int index = i;
                     int temp = array[index];
 
                     while (index - interval >= 0 && temp < array[index - interval]) {
-                        array[index] = array[index-interval];
-                        index -=interval;
+                        array[index] = array[index - interval];
+                        index -= interval;
                     }
                     array[index] = temp;
                 }
