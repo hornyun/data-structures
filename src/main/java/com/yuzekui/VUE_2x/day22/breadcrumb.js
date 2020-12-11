f281f8105eb87fcf35f63bd91fe20376b51e2997
yuzekui <154964320@qq.com>
2020-12-11 14:58:56 +0800

    vue练习，webpack入门

---
+++ src/main/java/com/yuzekui/VUE_2x/day22/breadcrumb.js
@@ -0,0 +1,19 @@
+/*
+<template>
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+        <el-breadcrumb-item :to="{path:'/'}">首页</el-breadcrumb-item>
+    <el-breadcrumb-item v-for="list in currentPath" >{{list}}</el-breadcrumb-item>
+</el-breadcrumb>
+</template>
+<style>
+
+
+</style>
+<script>
+    export default{
+    data(){
+    return {}
+},
+    props:['currentPath']
+}
+</script>*/
